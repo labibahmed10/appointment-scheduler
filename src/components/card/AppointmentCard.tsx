1b4043002ff567538985583e861feccfacab350e
labibahmed10 <labib.ahmed.372@gmail.com>
2024-08-12 02:35:02 +0600

    fix: appointment card shortening fixed based on date

--- src/components/card/AppointmentCard.tsx
+++ src/components/card/AppointmentCard.tsx
@@ -2,9 +2,10 @@ import { CalendarIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardFooter } from "../ui/card";
 import { format } from "date-fns";
-import { DocumentData } from "firebase/firestore";
+import { DocumentData, Timestamp } from "firebase/firestore";
 
 const AppointmentCard = ({ data }: DocumentData) => {
+  console.log();
   return (
     <Card className="w-full">
       <CardContent className="p-4">
@@ -15,7 +16,8 @@ const AppointmentCard = ({ data }: DocumentData) => {
             <div className="flex items-center gap-2">
               <CalendarIcon className="w-4 h-4 text-muted-foreground" />
 
-              <div className="">{`${format(data?.date?.toDate(), "PPP")} at ${data?.time}`}</div>
+              <div className="">{`
+              ${data.date instanceof Timestamp ? format(data?.date?.toDate(), "PPP") : format(data?.date, "PPP")} at ${data?.time}`}</div>
             </div>
           </div>
         </div>
